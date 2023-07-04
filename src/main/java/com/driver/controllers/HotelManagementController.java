702e1f35a1c5f55f95e03e296074f9d9e5eb70ac
khushboo <khushidudeja45@gmail.com>
2023-07-04 22:30:37 +0530

    7

--- src/main/java/com/driver/controllers/HotelManagementController.java
+++ src/main/java/com/driver/controllers/HotelManagementController.java
@@ -4,6 +4,7 @@ import com.driver.model.Booking;
 import com.driver.model.Facility;
 import com.driver.model.Hotel;
 import com.driver.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,9 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/hotel")
 public class HotelManagementController {
+
+    @Autowired
+    HotelManagementService hotelManagementService ;
 
     @PostMapping("/add-hotel")
     public String addHotel(@RequestBody Hotel hotel){
